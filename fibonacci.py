8747cb472610445cd18613662cecd4610d2d93cd
vmmaddel <64039230+vmmaddel@users.noreply.github.com>
2020-04-23 19:11:30 -0400

    intial files

---
+++ fibonacci.py
@@ -0,0 +1,4 @@
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return (fibonacci(n-2) + fibonacci(n-1))
---
+++ lucas.py
@@ -0,0 +1,6 @@
+def lucas(n):
+    if n == 0:
+        return 2
+    if n == 1:
+        return 1
+    return (lucas(n-2) + lucas(n-1))
